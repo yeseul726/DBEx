f39da922c6f167fbdb4b49ff29a1952661ec3af3
leeysi <yeseul726@e-mirim.hs.kr>
2017-06-13 20:33:55 +0900

    #3 Event Handling init Button

--- app/src/main/java/yeseul/kr/hs/emirim/dbex/MainActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/dbex/MainActivity.java
@@ -5,13 +5,40 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit_group_name, edit_group_count, edit_result_name, edit_result_count;
+    Button but_init, but_insert, but_select;
+    MyDBHelper myHelper;
+    SQLiteDatabase sqlDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit_group_name = (EditText)findViewById(R.id.edit_group_name);
+        edit_group_count = (EditText)findViewById(R.id.edit_group_count);
+        but_init = (Button)findViewById(R.id.but_init);
+        but_insert = (Button)findViewById(R.id.but_insert);
+        but_insert = (Button)findViewById(R.id.but_insert);
+        but_select = (Button)findViewById(R.id.but_select);
+        edit_result_name = (EditText)findViewById(R.id.edit_result_name);
+        edit_result_count = (EditText)findViewById(R.id.edit_result_count);
+
+        //DB 생성
+        myHelper = new MyDBHelper(this);
+        //기존의 테이블이 존재하면 삭제하고 테이블을 새로 생성한다.
+        but_init.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDb = myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDb, 1, 2);
+                sqlDb.close();
+            }
+        });
     }
 
     class MyDBHelper extends SQLiteOpenHelper{
